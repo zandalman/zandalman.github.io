cac7a2deb03b682ee6977004ab8f5870eb3944f7
zandalman <zandalman@gmail.com>
2023-09-04 08:52:39 -0400

    Added flask-frozen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
@@ -6,13 +7,15 @@ app = Flask(__name__)
 def render_index():
     return render_template("index.html")
 
-@app.route('/about')
+@app.route('/about.html')
 def render_about():
     return render_template("about.html")
 
-@app.route('/research')
+@app.route('/research.html')
 def render_research():
     return render_template("research.html")
 
+freezer = Freezer(app)
+
 if __name__ == '__main__':
-    app.run()
+        freezer.freeze()
